a3d5d9b4595ed9701c5906abbc71991702773c31
Tyler Neely <t@jujit.su>
2020-06-11 16:52:22 +0200

    Update phrasing for natsio crate deprecation

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
 //! A Rust client for the NATS.io ecosystem.
 //!
-//! We've moved! Please use the `nats` create instead of `natsio`.
-//! This crate is deprecated.
+//! We've moved! Please use the [`nats`](https://docs.rs/nats) crate instead of `natsio`. This
+//! crate is deprecated.
+//!
+//! We have continued developing and publishing [the same
+//! codebase](https://github.com/nats-io/nats.rs) from this crate under the `nats` crate.
